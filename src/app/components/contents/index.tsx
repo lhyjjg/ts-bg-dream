84b88907c1624f545c47a623b2f68ef759625e69
lhyjjg <leehyun147@gmail.com>
2023-12-20 00:08:02 +0900

    feat: 콘텐츠 영역 구현 1차

---
+++ src/app/components/contents/index.tsx
@@ -0,0 +1,40 @@
+import React, { useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Scrollbar } from "swiper/modules";
+import SwiperCore from "swiper";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import styles from "@/src/app/styles/content.module.css";
+
+interface GroupType {
+  img: string;
+  explanation: string;
+}
+
+interface ContentProps {
+  dataList: GroupType[];
+  title: string;
+}
+
+export default function Content({ dataList, title }: ContentProps) {
+  SwiperCore.use([Scrollbar]);
+  const swiperRef = useRef<SwiperCore>();
+
+  return (
+    <div className={styles.group}>
+      <h3 className={styles.title}>{title}</h3>
+      <Swiper className={styles.swiper_container} loop={true} slidesPerView={6}>
+        {dataList?.map((item, index) => (
+          <SwiperSlide key={index} className={styles.swiper_slide}>
+            <img src={item.img} alt={item.explanation} />
+            <div>
+              <h4 className={styles.explanation}>{item.explanation}</h4>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,18 @@
 
 import Header from "@/src/app/components/header/index";
 import Visual from "./components/visual";
+import Content from "./components/contents/index";
+import { group1, group2, group3 } from "@/public/assets/contents/group.js";
 
 export default function Home() {
+  // console.log(group1[0].img);
   return (
     <main>
       <Header />
       <Visual />
+      <Content dataList={group1} title="👒 따뜻한 봄볕이 그리울 때" />
+      <Content dataList={group2} title="🏖 까맣게 태워도 좋아" />
+      <Content dataList={group3} title="💼 나는야 차가운 도시의 여행자" />
     </main>
   );
 }
